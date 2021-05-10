94def9ae3da89f63f03554c675763dceadd2f243
Shujaat Azim <shujaat.azim@gmail.com>
2021-05-09 20:27:55 -0400

    Added router and removed unused files/code.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello, World</h1>
+    <div className="app">
+      <div className="header">
+
+      </div>
+      <div className="body">
+        <Switch>
+          <Route />
+        </Switch>
+      </div>
+      <div className="footer">
+
+      </div>
     </div>
   );
 }
