d11541a173c9549788ed2b7d135967d09d34760f
Shujaat Azim <shujaat.azim@gmail.com>
2021-05-13 20:55:55 -0400

    Created profile and login pages, will build them out with Bulma next.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import NotFoundPage from './Pages/NotFoundPage';
+import LoginPage from './Pages/LoginPage';
+import ProfilePage from './Pages/ProfilePage';
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
       <div className="body">
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/profile" component={ProfilePage}  />
           <Route component={NotFoundPage} />
         </Switch>
       </div>
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Card = props => {
   return (
-    <img style={{ height: "15rem", margin: "0rem 0.5rem 0.5rem 0.5rem" }} src={`https://images.pokemontcg.io/base1/${props.number}_hires.png`} alt="card" />
+    <img 
+      style={{ height: "15rem", margin: "0rem 0.5rem 0.5rem 0.5rem" }} 
+      src={`https://images.pokemontcg.io/base1/${props.number}_hires.png`} 
+      alt="card" 
+    />
   );
 }
+
 export default Card;
