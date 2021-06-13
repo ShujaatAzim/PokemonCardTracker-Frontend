5fb6b3f286d661cd62929f341cd7dff22e65bbff
Shujaat Azim <shujaat.azim@gmail.com>
2021-06-12 20:34:45 -0400

    Changed favicon.

--- src/Components/SetButtonsScreen.js
+++ src/Components/SetButtonsScreen.js
@@ -8,7 +8,7 @@ const SetButtonsScreen = props => {
   const { setSet } = props;
 
   return(
-    <div style={{ textAlign: "center", marginTop: "2rem" }}>
+    <div style={{ textAlign: "center" }}>
       <img src="https://fontmeme.com/permalink/210612/680c3a9e420df05eff3e08d6937137d0.png" alt="pokebook" border="0" />
       <h1>Welcome, {creds.username}!</h1>
       <h4>Choose a set:</h4>
