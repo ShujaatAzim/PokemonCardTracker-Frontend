97993f1a5ac776cd62dcf07a9cd397f406c919b7
Shujaat Azim <shujaat.azim@gmail.com>
2021-10-26 23:52:16 -0700

    Added home button to not found page.

--- src/Pages/NotFoundPage.js
+++ src/Pages/NotFoundPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 
 const NotFoundPage = () => {
+
+  let history = useHistory();
+
   return (
     <div>
       <p>Page Not Found!</p>
+      <button onClick={() => history.push('/')}>Home</button>
     </div>
   );
 }
