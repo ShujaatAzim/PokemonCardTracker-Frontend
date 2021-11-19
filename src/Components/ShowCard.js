b375c9a414bf0cb505c7af8256cdb5702930488a
Shujaat Azim <shujaat.azim@gmail.com>
2021-11-18 23:59:13 -0500

    Major update. Added new pages for sharing collections without giving the ability to change any of the info. If logged in, you will see your own collection and can also view other collections, logged in or not. Need to add separate home page though, as parent component for new show pages.

---
+++ src/Components/ShowCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ShowCard = props => {
+
+  const { card } = props
+
+  return (
+    <img 
+      style={{ height: "15rem", margin: "0rem 0.5rem 0.5rem 0.5rem", opacity: card.quantity !== 0 ? "100%" : "35%" }} 
+      src={card.image} 
+      alt="card" 
+      onClick={() => console.log(card)}
+    />
+  );
+}
+
+export default ShowCard;
--- src/Pages/CollectionSharePage.js
+++ src/Pages/CollectionSharePage.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router';
+import LoadingScreen from '../Components/LoadingScreen';
+import ShowCard from '../Components/ShowCard';
+import url from "../urlHelper";
 
 const CollectionSharePage = props => {
+  
+  const { set, setSet } = props;
+  
+  const location = useLocation()
+  const userID = (location.pathname.split("/")[2])
+
+  const [loading, setLoading] = useState(true)
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    getCollection()
+    setSet("Base Set")
+    // eslint-disable-next-line
+  }, [])
+
+ const getCollection = async () => {
+    try {
+      await fetch(`${url}/collection/${userID}`)
+      .then(resp => resp.json())
+      .then(data => setCards(data))
+      .then(() => setTimeout(() => setLoading(false), 100))
+    } catch {
+      console.log("error")
+    }
+  }
 
   return (
-    <div>
-      Collection share page!
+    loading || cards === [] ? <LoadingScreen /> : 
+    <div style={{ display: "flex" }}>
+      <div style={{ width: "65%" }}>
+        {cards.filter(card => card.set === set).map(card => <ShowCard key={card.id} card={card} unclickable={true} />)}
+      </div>
     </div>
   );
 }
