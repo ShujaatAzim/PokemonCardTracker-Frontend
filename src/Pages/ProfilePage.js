e3af6a35702f8ff3a9a8ee591c408667c47f5c3f
Shujaat Azim <shujaat.azim@gmail.com>
2021-12-01 23:16:03 -0800

    Added delete button for account to profile. Should probably disable it for the test account though.

--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
+import url from '../urlHelper';
 
 const ProfilePage = () => {
 
-  const creds = JSON.parse(localStorage.getItem("trackerCreds"))
+  const creds = JSON.parse(localStorage.getItem("trackerCreds"));
+  const history = useHistory();
+
+  const deleteAccount = e => {
+    e.preventDefault();
+    fetch(`${url}/users/${creds.id}`, {
+      method: "DELETE",
+      headers: {
+        "Authorization": `Bearer ${creds.jwt}`,
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+    })
+    .then(() => localStorage.removeItem("trackerCreds"))
+    .then(() => history.push('/'))
+  }
 
   // profile page should have generic card info
 
@@ -11,6 +29,10 @@ const ProfilePage = () => {
       <h1>{creds.username}'s Profile</h1>
       <h3>Share your collection with this link!</h3>
       <h3>https://pokebook.shujaatazim.com/collection/{creds.id}</h3>
+      <br />
+      <div>
+        <Button color="red" onClick={e => deleteAccount(e)}>Delete Account</Button>
+      </div>
     </div>
   );
 }
