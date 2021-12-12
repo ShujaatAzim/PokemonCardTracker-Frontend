e8b87ffe93676e2695cf0a3b53680647360f56db
Shujaat Azim <shujaat.azim@gmail.com>
2021-12-11 20:12:20 -1000

    Removed comments regarding the try it now button, since it has been added.

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -52,9 +52,6 @@ const LoginPage = props => {
     .then(() => history.push("/"))
   }
 
-  // need to add validations for username uniqueness instead of just on backend?
-  // also need to add instructions for logging in with test account, maybe a "try it now" button?
-
   return (
     <div style={{ textAlign: "center", marginLeft: "30rem", marginRight: "30rem" }}>
       <img src="https://fontmeme.com/permalink/210612/680c3a9e420df05eff3e08d6937137d0.png" alt="pokebook" border="0" />
