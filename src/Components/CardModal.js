8c49216872d47269feabae79e0bfeeaf38c411af
Shujaat Azim <shujaat.azim@gmail.com>
2022-01-16 21:11:18 -1000

    Built out modal more, with same features as cardinfo, which will be replaced.

--- src/Components/CardModal.js
+++ src/Components/CardModal.js
@@ -1,33 +1,43 @@
-import React from 'react';
-import { Button, Header, Image, Modal } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Button, Form, Header, Image, Modal, TextArea } from 'semantic-ui-react';
 
 const CardModal = props => {
 
+  const { card, open, setOpen } = props;
+
+  const [quantity, setQuantity] = useState(card.quantity)
+  const [notes, setNotes] = useState(card.notes)
+  const [updated, setUpdated] = useState(false)
+
+  const handleSubmit = () => {
+    console.log("submitted")
+  }
+
   return (
-    <Modal
-      onClose={() => props.setOpen(false)}
-      onOpen={() => props.setOpen(true)}
-      open={props.open}
-    >
-      <Modal.Header>{props.card.name}</Modal.Header>
+    <Modal onClose={() => setOpen(false)} onOpen={() => setOpen(true)} open={open}>
+      <Modal.Header>{card.name}</Modal.Header>
       <Modal.Content image>
-        <Image size='medium' src={props.card.image} wrapped />
+        <Image size='medium' src={card.image} wrapped />
         <Modal.Description>
-          <Header>{props.card.name} - {props.card.set}</Header>
-          <p>{props.card.notes}</p>
-          <p>Number owned: {props.card.quantity}</p>
-        <Button color="green">+</Button>
-        <Button color="red">-</Button>
+          <Header>{card.name} - {card.set}</Header>
+          <p>Notes:</p>
+          <Form>
+            <TextArea value={notes} rows={4} onChange={e => {setNotes(e.target.value);setUpdated(true)}}/>
+          </Form>
+          <p>Number owned: {quantity}</p>
+        <Button color="green" onClick={() => {setQuantity(quantity + 1);setUpdated(true)}}>+</Button>
+        <Button color="red" disabled={quantity < 1} onClick={() => {setQuantity(quantity - 1);setUpdated(true)}}>-</Button>
         </Modal.Description>
       </Modal.Content>
       <Modal.Actions>
-        <Button color='red' onClick={() => props.setOpen(false)}>Cancel</Button>
+        <Button color='red' onClick={() => setOpen(false)}>Cancel</Button>
         <Button
           content="Submit Changes"
           labelPosition='right'
           icon='checkmark'
-          onClick={() => props.setOpen(false)}
+          onClick={() => {setOpen(false);handleSubmit()}}
           positive
+          disabled={!updated}
         />
       </Modal.Actions>
     </Modal>
