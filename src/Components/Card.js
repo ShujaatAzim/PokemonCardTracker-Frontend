591cfaac4af1ce74caf4195470d7b2cc482f5b42
Shujaat Azim <shujaat.azim@gmail.com>
2023-07-05 20:39:00 -1000

    Auto lint card component.

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,39 +1,39 @@
-import React, { useState } from 'react';
-import CardModal from './CardModal';
+import React, { useState } from "react";
+import CardModal from "./CardModal";
 
-const Card = props => {
-
+const Card = (props) => {
   const { card, creds, getCards, canEdit, a, i } = props;
   const [open, setOpen] = useState(false);
 
   return (
     <React.Fragment>
-      <img 
+      <img
         className="card"
-        style={{ 
-          height: "15rem", 
-          margin: "0rem 0.5rem 0.5rem 0.5rem", 
-          opacity: a[i].quantity === 0 ? "40%" : "100%" 
-        }} 
-        src={a[i].image} 
-        alt={`${a[i].name}'s card pic from the ${a[i].set} set.`} 
+        style={{
+          height: "15rem",
+          margin: "0rem 0.5rem 0.5rem 0.5rem",
+          opacity: a[i].quantity === 0 ? "40%" : "100%",
+        }}
+        src={a[i].image}
+        alt={`${a[i].name}'s card pic from the ${a[i].set} set.`}
         onClick={() => setOpen(!open)}
-        />
-      {open ? 
-        <CardModal 
-          card={card} 
-          creds={creds} 
-          open={open} 
-          setOpen={setOpen} 
-          getCards={getCards} 
-          canEdit={canEdit} 
+      />
+      {open ? (
+        <CardModal
+          card={card}
+          creds={creds}
+          open={open}
+          setOpen={setOpen}
+          getCards={getCards}
+          canEdit={canEdit}
           count={props.count}
           setCount={props.setCount}
           i={props.i}
           a={props.a}
-        /> : null}
+        />
+      ) : null}
     </React.Fragment>
   );
-}
+};
 
 export default Card;
