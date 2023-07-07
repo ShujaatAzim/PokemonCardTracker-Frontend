9410f7cc7fc48410bacc62342063ad78a9496d65
Shujaat Azim <shujaat.azim@gmail.com>
2023-07-07 13:46:01 -1000

    Auto lint loading screen.

--- src/Components/LoadingScreen.js
+++ src/Components/LoadingScreen.js
@@ -1,22 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const LoadingScreen = props => {
-  
+const LoadingScreen = (props) => {
   const { loadingCode } = props;
 
   return (
     <div style={{ textAlign: "center" }}>
-      <img 
-        src="https://fontmeme.com/permalink/210612/680c3a9e420df05eff3e08d6937137d0.png" 
-        className="logo" 
-        alt="pokebook" 
-        border="0" 
+      <img
+        src="https://fontmeme.com/permalink/210612/680c3a9e420df05eff3e08d6937137d0.png"
+        className="logo"
+        alt="pokebook"
+        border="0"
       />
       <br />
       <br />
-      { loadingCode === "new user" ? "Creating new user..." : "Loading..." }
+      {loadingCode === "new user" ? "Creating new user..." : "Loading..."}
     </div>
   );
-}
+};
 
 export default LoadingScreen;
