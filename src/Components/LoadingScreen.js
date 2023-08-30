2c995d9c3de352656a3767ef14dcfcad1c741333
Shujaat Azim <shujaat.azim@gmail.com>
2023-08-30 03:48:00 -0400

    Updated styles for loading screen.

--- src/Components/LoadingScreen.js
+++ src/Components/LoadingScreen.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const LoadingScreen = props => {
+const LoadingScreen = (props) => {
   const { loadingCode } = props;
 
   return (
     <div style={{ textAlign: 'center' }}>
       <img
-        src="https://fontmeme.com/permalink/210612/680c3a9e420df05eff3e08d6937137d0.png"
-        className="logo"
-        alt="pokebook logo"
-        border="0"
+        src='https://fontmeme.com/permalink/210612/680c3a9e420df05eff3e08d6937137d0.png'
+        className='logo'
+        alt='pokebook logo'
+        border='0'
       />
       <br />
       <br />
