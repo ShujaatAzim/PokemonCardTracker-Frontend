e441b5838a0ce59f9c67d93580b5b32943815ce3
Shujaat Azim <shujaat.azim@gmail.com>
2024-01-01 21:03:17 -1000

    feat: Add comments and linting in urlHelper file.

--- src/urlHelper.js
+++ src/urlHelper.js
@@ -1,3 +1,6 @@
-const url = process.env.NODE_ENV === 'production' ? 'https://pb-backend.shujaatazim.com' : 'http://localhost:3000'
-
-export default url
+const url =
+  process.env.NODE_ENV === 'production'
+    ? 'https://pb-backend.shujaatazim.com'
+    : 'http://localhost:3000';
+// where does it get this info?
+export default url;
